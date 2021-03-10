9ec530fc45286a0d32470a52c4ebeb36778a0dba
koeunjun7 <koeunjun7@gmail.com>
2021-03-10 23:16:38 +0900

    3/10 commit

---
+++ Ps 1일 1문제/1463/1463.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){//dp 
+	int arr[1000001]; // 배열 크기 조건 잘보기 
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		arr[i]=99999;
+	}
+	arr[1]=0;
+	arr[2]=arr[3]=1;
+	for(int i=4;i<=n;i++){ //1 그냥 모든 경우 해보기-> 2. if문 줄이기 
+		arr[i]=arr[i-1]+1;
+		if(i%3==0) arr[i]=min(arr[i],arr[i/3]+1);
+		if(i%2==0) arr[i]=min(arr[i],arr[i/2]+1);
+	}
+	cout<<arr[n]<<"\n";
+}

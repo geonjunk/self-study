c7961bbb8294baa017ea7d741cdfddf55ecf3280
koeunjun7 <koeunjun7@gmail.com>
2021-04-02 21:32:56 +0900

    4/02 commit

---
+++ Ps 1일 n문제/210402/2606.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int graph[101][101];
+int visited[101];
+int v, e;//v수 e수
+int cp = 0;
+void dfs(int x) {
+	visited[x] = 1;
+	for (int i = 1; i <= v; i++) {
+		if (graph[x][i]&&!visited[i]) {
+			cp++;//시작점은 포함 안함
+			dfs(i);
+		}
+	}
+}
+int main() {
+	
+	int x, y;
+	cin >> v;
+	cin >> e;
+	for (int i = 0; i < e; i++) {
+		cin >> x >> y;
+		graph[x][y] = graph[y][x] = 1;
+	}
+	dfs(1);
+	cout << cp << "\n";
+
+
+	return 0;
+}
+//bfs + 인접리스트로도 해보기

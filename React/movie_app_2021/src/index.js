d29fccd9c9b17eb91a257e4a8f951bc03a0d7808
koeunjun7 <koeunjun7@gmail.com>
2021-08-27 23:37:50 +0900

    push nodejs chapter 7

--- React/movie_app_2021/src/index.js
+++ React/movie_app_2021/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
 ReactDOM.render(<App />,document.getElementById('root'));//그린다 (app 컴포넌트를, 아이디가 root인 element에)
 

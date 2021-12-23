b96f68e8b8a11053ecff42fa798c42b86aeb10c2
geonjunk <koeunjun7@gmail.com>
2021-12-23 15:12:26 +0900

    refact chapter 8 : mongoose version optimization

--- node.js/8장/learn-mongoose/schemas/index.js
+++ node.js/8장/learn-mongoose/schemas/index.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 
-const connect = () =>{
-    if(process.env.NODE_ENV!=='production'){
-        mongoose.set('debug',true);
+const connect = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    mongoose.set('debug', true);
+  }
+  mongoose.connect('db주소', {//6버전 부터 useNewUrlParser, useUnifiedTopology, and useCreateIndex 는 true, and useFindAndModify 는 false 로 항상 동작하므로 옵션 줄 필요없음
+    dbName: 'nodejs',
+   /* useNewUrlParser: true,
+    useCreateIndex: true,*/
+  }, (error) => {
+    if (error) {
+      console.log('몽고디비 연결 에러', error);
+    } else {
+      console.log('몽고디비 연결 성공');
     }
-    mongoose.connect('mongodb://이름:비밀번호@localhost:27017/admin',{
-        dbName:'nodejs',
-        useNewUrlParser:true,
-        useCreateIndex: true,
-    },(error)=>{
-        if(error){
-            console.log('몽고디비 연결 에러',error);
-        }else{
-            console.log('몽고디비 연결 성공');
-        }
-    })
-
+  });
 };
-mongoose.connection.on('error',(error)=>{
-    console.log('몽고디비 연결 에러',error);
+
+mongoose.connection.on('error', (error) => {
+  console.error('몽고디비 연결 에러', error);
 });
-mongoose.connection.on('disconnected',()=>{
-    console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
-    connect();
+mongoose.connection.on('disconnected', () => {
+  console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.');
+  connect();
 });
 
-module.exports=connect;
+module.exports = connect;

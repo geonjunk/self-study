0a2e418ed810d3e1b7fc9bfcd2b41bfb069d966a
geonjunk <koeunjun7@gmail.com>
2022-09-09 00:03:46 +0900

    spring boot /spring : 김영한님 강의 3강 실습

--- spring-springboot/spring concept/core/core/src/main/java/hello/core/MemberApp.java
+++ spring-springboot/spring concept/core/core/src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService=new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //spring container라고 보면 됨, Bean으로 등록한 객체 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);//(메서드이름,타입)
+
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
 

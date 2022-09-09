bac56e1f45e3030996cfc65ea76fe7046cf80d2f
geonjunk <koeunjun7@gmail.com>
2022-09-09 17:56:04 +0900

    spring / spring boot : 김영한님 강의 4강 실습

---
+++ spring-springboot/spring concept/core/core/src/test/java/hello/core/beanfind/ApplicationContextTest.java
@@ -0,0 +1,39 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationlBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            //beanDefinition : bean 하나하나에 대한 메타데이터 정보
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                // 스프링 내부에서 무언가 하길위해 등록한게 아닌 내가 개발하기 위해 등록한 것들
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+
+}
